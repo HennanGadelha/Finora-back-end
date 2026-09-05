9f1ade41bbe0e90d28267438e311fc7c6e0634ca
hennangadelha <hennanhennan@gmail.com>
2026-09-05 18:36:27 -0300

    chore: inicializa backend do Finora

---
+++ src/test/java/com/finora_app/finora/FinoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finora_app.finora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

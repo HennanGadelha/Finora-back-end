70ae5f511befd0aeff71677ae9f4253618dd8566
hennangadelha <hennanhennan@gmail.com>
2026-09-06 14:06:26 -0300

    Adicionar documentacao Swagger OpenAPI

--- src/main/java/com/finora_app/finora/application/usuario/CadastroUsuarioRequest.java
+++ src/main/java/com/finora_app/finora/application/usuario/CadastroUsuarioRequest.java
@@ -1,4 +1,7 @@
 package com.finora_app.finora.application.usuario;
 
-public record CadastroUsuarioRequest(String nome, String email, String senha) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record CadastroUsuarioRequest(String nome, String email,
+		@Schema(accessMode = Schema.AccessMode.WRITE_ONLY) String senha) {
 }
---
+++ src/main/java/com/finora_app/finora/config/OpenApiConfiguration.java
@@ -0,0 +1,12 @@
+package com.finora_app.finora.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(info = @Info(title = "Finora API", version = "v1", description = "API de gerenciamento financeiro do Finora"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
+public class OpenApiConfiguration {
+}
